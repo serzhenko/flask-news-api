600c7539a8e74b465ca82496a08c75cd1cd53377
Dmitry Serzhenko <dmitry@serzhenko.ru>
2023-03-13 18:06:04 +0300

    update

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from flask import Flask
-from data import db_session
-from data import db_session, news_api
+from flask import Flask, make_response, jsonify
+from flask_restful import Api
+
+from data import db_session, news_resources
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-
+api = Api(app, catch_all_404s=True)
 
 
 def main():
     db_session.global_init("db/blogs.db")
-    app.register_blueprint(news_api.blueprint)
+    # для списка объектов
+    api.add_resource(news_resources.NewsListResource, '/api/v2/news')
+
+    # для одного объекта
+    api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
+
     app.run()
 
 
