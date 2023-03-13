c892f50a7a5a71c3150d88301b54fb0800edcefd
Dmitry Serzhenko <dmitry@serzhenko.ru>
2023-03-13 17:28:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from data import db_session
+from data import db_session, news_api
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.register_blueprint(news_api.blueprint)
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
